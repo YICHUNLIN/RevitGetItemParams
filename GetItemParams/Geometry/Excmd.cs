196bf28cd958db58c86c4a36f40a86f8d4f0a39d
YICHUNLIN <yichun03@gmail.com>
2017-06-05 17:55:24 +0800

    join v2

--- GetItemParams/Geometry/Excmd.cs
+++ GetItemParams/Geometry/Excmd.cs
@@ -13,28 +13,91 @@ namespace Geometry
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            
+
             // 柱 -> 梁 -> 版 -> 牆
             String column = "結構柱";
             String beam = "結構構架";
+            String beamsystem = "結構樑系統";
             String board = "樓板";
 
             UIDocument uidoc = commandData.Application.ActiveUIDocument;
             Document doc = uidoc.Document;
             ICollection<ElementId> elemids = uidoc.Selection.GetElementIds();
             MessageBox.Show("You select " + elemids.Count.ToString());
-
+            
             Transaction trans = new Transaction(doc);
-            foreach (ElementId fid in elemids){
-                foreach(ElementId sid in elemids)
+            foreach (ElementId fid in elemids)
+            {
+                Element firstElement = doc.GetElement(fid);
+                BoundingBoxXYZ bbx = firstElement.get_BoundingBox(null);
+                Outline outline = new Outline(bbx.Min, bbx.Max);
+                BoundingBoxIntersectsFilter invertFilter = new BoundingBoxIntersectsFilter(outline, false);
+                IList<Element> noIntersects = new FilteredElementCollector(doc).WherePasses(invertFilter).ToElements();
+                foreach(Element secondElement in noIntersects)
+                {
+                    try
+                    {
+                        String fn = firstElement.Category.Name;
+                        String sn = secondElement.Category.Name;
+                        trans.Start("join");
+                        if (fn == column)
+                        {
+                            if (sn != column)
+                                try
+                                {
+                                    if (!JoinGeometryUtils.AreElementsJoined(doc, firstElement, secondElement))
+                                        JoinGeometryUtils.JoinGeometry(doc, firstElement, secondElement);
+                                }
+                                catch (Exception e)
+                                {
+                                    //MessageBox.Show("1"+e.ToString());
+                                }
+
+                        }
+                        else if (fn == beam)
+                        {
+                            if (sn != beam && sn != column)
+                                try
+                                {
+                                    if (!JoinGeometryUtils.AreElementsJoined(doc, firstElement, secondElement))
+                                        JoinGeometryUtils.JoinGeometry(doc, firstElement, secondElement);
+                                }
+                                catch (Exception e)
+                                {
+                                    //MessageBox.Show("2"+e.ToString());
+                                }
+                        }
+                        else if (fn == board)
+                        {
+                            if (sn != board && sn != column && sn != beam)
+                                try
+                                {
+                                    if (!JoinGeometryUtils.AreElementsJoined(doc, firstElement, secondElement))
+                                        JoinGeometryUtils.JoinGeometry(doc, firstElement, secondElement);
+                                }
+                                catch (Exception e)
+                                {
+                                    // MessageBox.Show("3" + e.ToString());
+
+                                }
+
+                        }
+                        else
+                        {
+
+                        }
+                        trans.Commit();
+                    } catch (Exception e) {
+
+                    }
+                }
+                /*foreach (Element sid in noIntersects)
                 {
 
-                    if (fid.IntegerValue != sid.IntegerValue)
-                    {
-
-                        Element firstElement = doc.GetElement(fid);
-                        Element secondElement = doc.GetElement(sid);
-
+                    // if (fid.IntegerValue != sid.IntegerValue)
+                    // {
+                    //Element secondElement = doc.GetElement(sid);
+                        Element secondElement = sid;
                         String fn = firstElement.Category.Name;
                         String sn = secondElement.Category.Name;
                         trans.Start("join");
@@ -87,48 +150,15 @@ namespace Geometry
                         }
                         trans.Commit();
 
-                    }
-                }
+                    //}
+                }*/
 
             }
-            /*
-            UIApplication uiapp = commandData.Application;
-            UIDocument uidoc = uiapp.ActiveUIDocument;
-            //Application app = uiapp.Application;
-            Document doc = uidoc.Document;
+          
 
-            // get all walls on the active view
-            FilteredElementCollector collWalls =
-              new FilteredElementCollector(doc, doc.ActiveView.Id);
-            collWalls.OfClass(typeof(Wall));
 
-            foreach (Wall w in collWalls)
-            {
-                // get columns on the active view
-                FilteredElementCollector collColumnsOnThisWall =
-                  new FilteredElementCollector(doc, doc.ActiveView.Id);
-                collColumnsOnThisWall.OfClass(typeof(FamilyInstance));
-                collColumnsOnThisWall.OfCategory(
-                  BuiltInCategory.OST_StructuralColumns);
-
-                // as we don't want all columns, let's filter
-                // by the wall bounding box (intersect)
-                BoundingBoxXYZ bb = w.get_BoundingBox(doc.ActiveView);
-                Outline outline = new Outline(bb.Min, bb.Max);
-                BoundingBoxIntersectsFilter bbfilter =
-                  new BoundingBoxIntersectsFilter(outline);
-                collColumnsOnThisWall.WherePasses(bbfilter);
-
-                // finally, call JOIN on the wall and column
-                foreach (FamilyInstance column in collColumnsOnThisWall)
-                {
-                    JoinGeometryUtils.JoinGeometry(doc, w, column);
-                }
-            }
-            */
             return Result.Succeeded;
         }
-
 
     }
 }
